02630f5444304bcf40f06d857f54c569c7a928a7
Matthew Kukulski <Matthew.Kukulski@du.edu>
2020-12-03 00:47:44 -0700

    JPL Mars Space Image VSC scraping

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -15,6 +15,7 @@ def scrape():
     mars_dict ={}
 
     # Mars News URL page to be scraped
+
     url = "https://mars.nasa.gov/news/"
     browser.visit(url)
     html = browser.html
@@ -22,5 +23,22 @@ def scrape():
     # Collect the latest News Title and Paragraph Text
     news_title = soup.find_all('div', class_='content_title')[0].text
     news_p = soup.find_all('div', class_='article_teaser_body')[0].text
+
+    # JPL Mars Space Image to be scraped
+
+    space_image_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(space_image_url)
+    html_image = browser.html
+    soup = bs(html_image, "html.parser")
+    # Retrieve background-image url
+    image_path = soup.find_all('img')[3]["src"]
+    # Website Url 
+    main_url = "https://www.jpl.nasa.gov"
+    # Concatenate website url with scrapped route
+    featured_image_url = main_url + image_path
+
+    # Mars Facts to be scraped
+
+    
 
     return mars_dict
