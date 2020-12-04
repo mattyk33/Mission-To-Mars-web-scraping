0d07545ae7e75d63f5d466a9a403fb3e3909d020
Matthew Kukulski <Matthew.Kukulski@du.edu>
2020-12-03 19:59:01 -0700

    flask_app initial setup

---
+++ Missions_to_Mars/flask_app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection locally 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app
