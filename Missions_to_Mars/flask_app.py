6fa0ffd889be2f9817f13042aab4c2c1463498c9
Matthew Kukulski <Matthew.Kukulski@du.edu>
2020-12-04 21:10:47 -0700

    flask app index route

--- Missions_to_Mars/flask_app.py
+++ Missions_to_Mars/flask_app.py
@@ -8,3 +8,23 @@ app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection locally 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def index():
+
+    # Find data from the mongo database
+    mars_dict = mongo.db.mars_dict.find_one()
+    # Return template and data
+    return render_template("index.html", mars_dict=mars_dict)
+
+# Route for scrape function
+@app.route("/scrape")
+def scrape():
+
+    
+
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True
